a35c8314c6d8bb375ef221f4c2217a985e6492eb
Jhon Jaime Quintero <jquintero9@JHON-PC>
2017-05-07 20:44:59 -0500

    .

--- manage_shoes/urls.py
+++ manage_shoes/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('usuario.urls', namespace='usuario')),
+    #url(r'', include('usuario.urls', namespace='usuario')),
 ]
